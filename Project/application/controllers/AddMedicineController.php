98475e6a7b1421ea3720ee71e73137014b1b7021
thejas28 <57125421+thejas28@users.noreply.github.com>
2022-10-10 16:17:19 -0400

    Add files via upload
    Signed-off-by: thejas28 <57125421+thejas28@users.noreply.github.com>

---
+++ Project/application/controllers/AddMedicineController.php
@@ -0,0 +1,59 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: sanathls
+ * Date: 29/09/19
+ * Time: 4:37 PM
+ */
+
+require_once __DIR__.'/../models/Medicine.php';
+
+if(isset($_POST['submit']))
+{
+    $addMedObj = new AddMedicineController();
+    $addMedObj->getUserInput();
+}
+
+class AddMedicineController
+{
+    public function getUserInput()
+    {
+        $name=$_POST['m_name'];
+        $category=$_POST['m_category'];
+        $description=$_POST['m_description'];
+        $this->addMedicine($name,$category,$description);
+    }
+
+    public function addMedicine($name,$category,$description)
+    {
+        if($name==null || $category==null || $description==null)
+        {
+            echo "
+                <script>
+                        alert('Please Enter Medicine Name Category And Description');
+                        window.location.href='../views/AddMedicine.php';
+                </script>";
+            return;
+        }
+        $medObj= new Medicine();
+        $res =$medObj->addMedicine($name,$category,$description);
+
+        if($res)
+        {
+            echo "
+                <script>
+                        alert('Medicine Added Successfully');
+                        window.location.href='../views/AddMedicine.php';
+                </script>";
+            //redirect to all suppliers page if time permits
+        }
+        else
+        {
+            echo "
+                <script>
+                        alert('Add Medicine Failed');
+                        window.location.href='../views/AddMedicine.php';
+                </script>";
+        }
+    }
+}
